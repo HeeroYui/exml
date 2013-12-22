5f2ae21798cd2967e649dc3520ea667408ad48c6
Edouard DUPIN <yui.heero@gmail.com>
2013-12-22 19:03:35 +0100

    [DEV] add monk documentation

---
+++ monk_exml.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import monkModule as module
+import monkTools as tools
+
+def create():
+	# module name is 'edn' and type binary.
+	myModule = module.Module(__file__, 'exml', 'LIBRARY')
+	# enable doculentation :
+	myModule.set_website("http://heeroyui.github.io/exml/")
+	myModule.set_path(tools.get_current_path(__file__) + "/exml/")
+	# add the currrent module at the 
+	return myModule
+
+def get_desc():
+	return ""
