a420811f5928bf2a972aace1df8898079dc43abf
Edouard DUPIN <yui.heero@gmail.com>
2013-12-28 16:10:01 +0100

    [DOC] add monk website

--- monk_exml.py
+++ monk_exml.py
@@ -10,6 +10,7 @@ def create():
 	myModule = module.Module(__file__, 'exml', 'LIBRARY')
 	# enable doculentation :
 	myModule.set_website("http://heeroyui.github.io/exml/")
+	myModule.set_website_sources("http://github.com/heeroyui/exml/")
 	myModule.set_path(tools.get_current_path(__file__) + "/exml/")
 	# add the currrent module at the 
 	return myModule
